4d1010e795ac5561aeb4588c32ab84781f172fc5
Suzy Le Bel <suzy.lebel@gmail.com>
2020-09-30 17:04:30 -0700

    tutoring

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 var currentQuestion = 0;
 const timer = document.querySelector('h1');
-var startButton = document.getElementById("#startbutton");
-var maincontainer = document.getElementById("#maincontainer");
-const questionsEl = document.getElementById("#questions");
-const answerEl = document.getElementById("#answers");
-const questionContainerEl = document.getElementById("#questionContainer");
+var startButton = document.getElementById("startbutton");
+var maincontainer = document.getElementById("maincontainer");
+const questionsEl = document.getElementById("questions");
+const answerEl = document.getElementById("answers");
+const questionContainerEl = document.getElementById("questionContainer");
 // var button = document.getElementById("#button");
 // button.addEventListener("click", function(setInterval){
 // });
@@ -38,16 +38,16 @@ function hide() {
 
 var quizArr = [
   {
-    "questionTitle": "Question 1?",
-    "multipleChoice": ["A", "B", "C", "D"],
+    "questionTitle": "Which of these is not a JavaScript data type?",
+    "multipleChoice": ["Number", "Element", "Object", "Boolean"],
     "answer": "B",
 
   },
 
   {
-    "questionTitle": "Question 2?",
-    "multipleChoice": ["A", "B", "C", "D"],
-    "answer": "D",
+    "questionTitle": "The Dom is an application programming interface valid for ?",
+    "multipleChoice": ["Visual Studio Code", "Goggle Chrome", "CSS", "HTML"],
+    "answer": "HTML",
 
   },
 
@@ -68,17 +68,19 @@ var quizArr = [
 
 function displayQuestion(qstion, choice) {
   console.log("questions display here");
-  // questionsEl.innerHTML = "";
+  questionsEl.textContent = "";
   var questionTitle = document.createElement("p");
   var node = document.createTextNode(qstion);
   questionTitle.appendChild(node);
-  // questionsEl.appendChild(questionTitle);
+  questionsEl.appendChild(questionTitle);
 
-  var choiceA = document.createElement("li");
-  var choiceB = document.createElement("li");
-  var choiceC = document.createElement("li");
-  var choiceD = document.createElement("li");
+
+  var choiceA = document.createElement("button");
+  var choiceB = document.createElement("button");
+  var choiceC = document.createElement("button");
+  var choiceD = document.createElement("button");
   var questiontitlebox = document.createElement("ol");
+  
 
   var nodeA = document.createTextNode(choice[0]);
   choiceA.appendChild(nodeA);
@@ -89,7 +91,7 @@ function displayQuestion(qstion, choice) {
   var nodeD = document.createTextNode(choice[3]);
   choiceD.appendChild(nodeD);
 
-  questionsEl.appendChild(questiontitlebox);
+  questionContainerEl.appendChild(questiontitlebox);
   questiontitlebox.appendChild(choiceA);
   questiontitlebox.appendChild(choiceB);
   questiontitlebox.appendChild(choiceC);
@@ -98,27 +100,41 @@ function displayQuestion(qstion, choice) {
 
 };
 
+questionsEl.onclick =  function (event) {
+  let li = event.target.closest("li");
+  if (!li) return;
+  if (!questionsEl.contains(li)) return;
+  if (li.textContent === quizArr[currentQuestion].answer) {
+    currentQuestion++;
+    playQuiz();
+  // } else {
+  //   alert("game over")
+  }
+}
 
-function runQuiz() {
-  if (startingquestion < quizArr.length) {
+function playQuiz() {
+  if (currentQuestion < quizArr.length) {
     displayQuestion(
-      quizArr[startingquestion].questionTitle,
-      quizArr[startingquestion].multipleChoice
+      quizArr[currentQuestion].questionTitle,
+      quizArr[currentQuestion].multipleChoice
+
+      // game mechanics (while loop) 
+
     );
   } else {
     alert("Game over")
   }
 }
 
-function selectAnswer() {
+// function selectAnswer() {
 
-}
+// }
 
 
 // Event Listeners
 document.getElementById("startbutton").addEventListener("click", startTimer);
 document.getElementById("startbutton").addEventListener("click", hide);
-document.getElementById("startbutton").addEventListener("click", displayQuestion);
+document.getElementById("startbutton").addEventListener("click", playQuiz);
 // document.getElementById("startbutton").addEventListener("click, ____")
 
 
